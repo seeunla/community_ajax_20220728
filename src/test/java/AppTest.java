96e79ef2b6a7fc90e1528393eebf54bbee7dc1b5
seeunla <ksu98712@gmail.com>
2022-07-28 13:08:03 +0900

    리스트와 맵을 json화

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,6 +3,10 @@ import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
@@ -22,6 +26,29 @@ public class AppTest {
                 """.trim());
     }
 
+    @Test
+    void ObjectMapper__articleDtoListToJsonStr() throws JsonProcessingException {
+       List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDtos.add(new ArticleDto(1, "제목", "내용"));
+        articleDtos.add(new ArticleDto(2, "제목2", "내용2"));
+
+        String jsonStr = Ut.json.toJonStr(articleDtos, "");
+        assertThat(jsonStr).isEqualTo("""
+                [{"id":1,"title":"제목","body":"내용"},{"id":2,"title":"제목2","body":"내용2"}]
+                """.trim());
+    }
+
+    @Test
+    void ObjectMapper__articleDtoMapToJsonStr() {
+        HashMap<String, ArticleDto> articleDtoMap = new HashMap<>();
+        articleDtoMap.put("최신",new ArticleDto(1, "제목", "내용"));
+        articleDtoMap.put("가장오래된", new ArticleDto(2, "제목2", "내용2"));
+
+        String jsonStr = Ut.json.toJonStr(articleDtoMap, "");
+        assertThat(jsonStr).isEqualTo("""
+                {"가장오래된":{"id":2,"title":"제목2","body":"내용2"},"최신":{"id":1,"title":"제목","body":"내용"}}
+                """.trim());
+    }
     @Test
     void ObjectMapper__jsonStrToObj() {
         ArticleDto articleDtoOrigin = new ArticleDto(1, "제목", "내용");
