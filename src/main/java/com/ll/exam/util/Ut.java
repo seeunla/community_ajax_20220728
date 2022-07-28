c4580aff6a9ebdc88681acdba4dabb6806ca46e8
seeunla <ksu98712@gmail.com>
2022-07-28 11:38:34 +0900

    json을 다시 객체화

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -2,14 +2,27 @@ package com.ll.exam.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
 
 public class Ut {
     public static class json {
-        public static String toJonStr(Object obj, String defaultValue) {
-            ObjectMapper om = new ObjectMapper();
+        private static final ObjectMapper om;
 
+        static {
+            om = new ObjectMapper();
+        }
+
+        public static String toJonStr(Object obj, String defaultValue) {
             try {
                 return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static Object toObj(String jsonStr, Class cls, Object defaultValue) {
+            try {
+                return om.readValue(jsonStr, cls);
             } catch (JsonProcessingException e) {
                 return defaultValue;
             }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
@@ -11,15 +12,23 @@ public class AppTest {
         assertThat(rs).isEqualTo(30);
     }
     @Test
-    void 실험_ObjectMapper() {
-
-
+    void ObjectMapper__ObjToJsonStr() throws JsonProcessingException {
         ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
 
         String jsonStr = Ut.json.toJonStr(articleDto, "");
         assertThat(jsonStr).isNotBlank();
         assertThat(jsonStr).isEqualTo("""
-                {"id":1, "title": "제목", "body" : "내용"}
+                {"id":1,"title":"제목","body":"내용"}
                 """.trim());
+    }
+
+    @Test
+    void ObjectMapper__jsonStrToObj() throws JsonProcessingException {
+        ArticleDto articleDtoOrigin = new ArticleDto(1, "제목", "내용");
+        String jsonStr = Ut.json.toJonStr(articleDtoOrigin, "");
+
+        ArticleDto articleDtoFromJson = (ArticleDto) Ut.json.toObj(jsonStr, ArticleDto.class, null);
+
+        assertThat(articleDtoOrigin).isEqualTo(articleDtoFromJson);
     }
 }
