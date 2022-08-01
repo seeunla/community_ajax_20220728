e523dc99680625416c180233e550b196f14ef914
seeunla <ksu98712@gmail.com>
2022-08-01 10:02:49 +0900

    채팅방 페이지, 채팅메세지 등록기능

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -30,6 +30,9 @@ public class DispatchServlet extends HttpServlet {
                     case "/usr/chat/roomList":
                         chatController.showRoomList(rq);
                         break;
+                    case "/usr/chat/room":
+                        chatController.showRoom(rq);
+                        break;
                     case "/usr/article/modify":
                         articleController.showModify(rq);
                         break;
@@ -52,6 +55,9 @@ public class DispatchServlet extends HttpServlet {
                 break;
             case "POST":
                 switch (rq.getActionPath()) {
+                    case "/usr/chat/writeMessage":
+                        chatController.doWriteMessage(rq);
+                        break;
                     case "/usr/chat/createRoom":
                         chatController.doCreateRoom(rq);
                         break;
